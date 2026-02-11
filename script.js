eb825585571296f41d4ce5afefb4e397db278b62
brokartell <Bro.Kartell@gmail.com>
2026-02-11 19:30:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -280,3 +280,14 @@ window.addEventListener("resize", () => {
     btn.href = `mailto:${email}`;
   }
 })();
+// Mail für Impressum / Datenschutz
+(() => {
+  const user = "post";
+  const domain = "bro-kartell.com";
+  const email = `${user}@${domain}`;
+
+  const btnLegal = document.getElementById("mailBtnLegal");
+  if (btnLegal) {
+    btnLegal.href = `mailto:${email}`;
+  }
+})();
